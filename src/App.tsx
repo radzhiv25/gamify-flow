4c29cb741530251a4e579dccfaa4878f9670f7fc
radzhiv <radzhivkrishna@gmail.com>
2026-03-30 13:02:53 +0530

    React app initialized with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className=" font-medium text-2xl">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
